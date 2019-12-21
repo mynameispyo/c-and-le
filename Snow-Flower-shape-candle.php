dab1b681ec570f9e60c9122d18ab656a4ccef02b
mynameispyo <54756611+mynameispyo@users.noreply.github.com>
2019-12-20 23:53:18 -0500

    Add files via upload

---
+++ Snow-Flower-shape-candle.php
@@ -0,0 +1,8 @@
+<form method=get name=form action=/buy/1.php>
+
+<input type=hidden name=product value="snow">
+
+<script>document.form.submit();</script>
+
+</form>
+<meta http-equiv='refresh' content='0;url=/buy/1.php'>
